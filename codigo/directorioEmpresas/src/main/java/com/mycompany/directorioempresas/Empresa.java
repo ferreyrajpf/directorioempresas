b9666d37c57aef1fa9f54e0afb1c7f15ba406968
Juan Pablo <jpferreyra@sanfrancisco.utn.edu.ar>
2021-04-30 18:35:57 -0300

    definición de modelos y codificación de clases

---
+++ codigo/directorioEmpresas/src/main/java/com/mycompany/directorioempresas/Empresa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.directorioempresas;
+
+/**
+ *
+ * @author juan
+ */
+public class Empresa {
+    private String razonSocial;
+    private String email;
+    private String telefono;
+    
+}
---
+++ codigo/directorioEmpresas/src/main/java/com/mycompany/directorioempresas/Producto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.directorioempresas;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author juan
+ */
+public class Producto {
+    private String nombre;
+    private String descripcion;
+    private BigDecimal precio = new BigDecimal(0.0);
+}
